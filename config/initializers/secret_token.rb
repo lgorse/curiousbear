017130c60801871d3562393e63524b33d42692e2
lgorse <lgorse@stanford.edu>
2013-02-27 21:28:17 -0800

    Basic files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curiousbear::Application.config.secret_token = '8ff5625823ce4f9840c1098474f09e3e385da2d15f093bac6ce216078b7df8ef1db2a67800778ed99a5673e28724e49ce755bab97d58be913743a20bda7ddfd4'
