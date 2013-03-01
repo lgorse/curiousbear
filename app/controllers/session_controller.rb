6dbad0009f038f5e6f34e257deddc5850424def0
lgorse <lgorse@stanford.edu>
2013-03-01 08:49:28 -0800

    Instated session fb_cookie and reset login to basic Facebook

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,25 +1,26 @@
 class SessionController < ApplicationController
 	
-	before_filter :parse_facebook_cookies, :except=>[:sigin]
+	before_filter :parse_facebook_cookies, :except=>[:signin]
 
 	def parse_facebook_cookies
-		@facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
-		session['fb_cookie'] = @facebook_cookies if @facebook_cookies.present?
+		session['fb_cookie'] ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
+		 #= @facebook_cookies if @facebook_cookies.present?
 	end
 
 	def signin
+		redirect_to welcome_path if session['fb_cookie'].present?
 		
 	end
 
 	
 	def welcome
-		@access_token = @facebook_cookies["access_token"]
+		@access_token = session['fb_cookie']["access_token"]
 		@graph = Koala::Facebook::GraphAPI.new(@access_token)
 		@me = @graph.get_object("me")
 	end
 
 	def super
-		@access_token = @facebook_cookies["access_token"]
+		@access_token = session['fb_cookie']["access_token"]
 		@graph = Koala::Facebook::GraphAPI.new(@access_token)
 		@hero = @graph.get_object("me")
 	end
