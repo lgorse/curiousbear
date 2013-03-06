e1383ca62ddedacb14e859713dc5b9eb9e6f91ba
lgorse <lgorse@stanford.edu>
2013-03-05 23:59:11 -0800

    added restaurant model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 
-
+before_filter :authenticate
 def index
 	@client = GooglePlaces::Client.new(GOOGLE_API_KEY)
 	@result = @client.spots_by_query(params[:search], :sensor => false, :types => ['restaurant', 'food'], :radius => 20000)
