2947332da2670aabfd6956732f3f2ba1ee85e50c
lgorse <lgorse@stanford.edu>
2013-03-13 20:58:42 -0700

    Added error handling for reviews and flash messaging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,11 @@ class ReviewsController < ApplicationController
 		@venue = JSON.parse(URI.decode(Base64.urlsafe_decode64(params[:review][:venue])))
 		@restaurant = Restaurant.find_or_create_by_google_id(@venue["google_id"], final_restaurant_attributes(@venue))
 		@attr = set_review_attributes(params[:review], @restaurant.id, @current_user.id)
-		Review.create!(@attr)
+		if review = Review.create(@attr)
+			flash[:success] = "Review saved. Share with friends!"
+		else
+			flash[:error] = review.errors.full_messages.to_sentence
+		end
 		redirect_to restaurant_path(@restaurant)
 	end
 
@@ -27,9 +31,17 @@ class ReviewsController < ApplicationController
 		@restaurant = Restaurant.find(@review.restaurant_id)
 		if @review.update_attributes(params[:review])
 			flash[:success] = "Review updated"
-			redirect_to @restaurant
+		else
+			flash[:error] = @review.errors.full_messages.to_sentence
 		end
+		redirect_to @restaurant
+	end
 
+	def destroy
+		@review = Review.find(params[:id])
+		@restaurant = Restaurant.find(@review.restaurant_id)
+		@review.destroy
+		redirect_to restaurant_path(@restaurant)
 	end
 
 end
