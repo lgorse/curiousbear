56d8b2f9623af8d2b9c924f023de3eff870e5a27
lgorse <lgorse@stanford.edu>
2013-03-14 08:13:42 -0700

    Implemented shared JS in sitewide folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,22 +15,7 @@
 //= require_tree .
 //= require gmap3
 
-$(function() {
-	$('.flash').fadeIn('normal', function() {
-		$(this).delay(2000).animate({opacity: 0.0}, 500, 'linear');
-	});
 
-});
-
-$(function(){
-	$("#share_review").click(function(){
-		if (this.checked){
-			alert("It's checked");
-		}else{
-			alert("Not checked");
-		}
-	});
-});
 
 
 
