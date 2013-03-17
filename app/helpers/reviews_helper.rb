aa8ac812133fe2a9e48f9cccfe6f039644de6be1
lgorse <lgorse@stanford.edu>
2013-03-16 17:27:58 -0700

    Implemented google maps for rview list. Stopped before had to change ID  system for map (reason: considering not doing this because of Google API expense)

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -9,4 +9,11 @@ module ReviewsHelper
 		}
 
 	end
+
+	def set_review_google_info(review)
+		@reference = review.restaurant.google_reference
+		@venue = get_restaurant_from_reference
+		@lat = @venue["geometry"]["location"]["lat"]
+		@lng = @venue["geometry"]["location"]["lng"]
+	end
 end
