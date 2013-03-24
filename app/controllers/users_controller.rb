298ac46a77a3c7600d7e1a22ec6ff63a5ebf1a9d
lgorse <lgorse@stanford.edu>
2013-03-24 12:00:53 -0700

    Limited pagination to 5 reviews on user reviews to avoid over_query_limit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ end
 
 def show
 	@user = User.find(params[:id])
-	@reviews = @user.reviews.paginate(:page => params[:page], :per_page => 10)
+	@reviews = @user.reviews.paginate(:page => params[:page], :per_page => 5)
 	respond_to do |format|
 		format.html
 		format.js
