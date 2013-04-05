b08c6d856f7024a0ba17839abe69e012454875b3
lgorse <lgorse@stanford.edu>
2013-04-05 00:36:44 -0700

    Removed the google types from url_params in parse_google_search

--- config/initializers/google_api.rb
+++ config/initializers/google_api.rb
@@ -1,3 +1,2 @@
-GOOGLE_API_KEY = "AIzaSyAu3PESBLfwBXKo8511DT78Wg0-S8aNZUU"
+GOOGLE_API_KEY = "AIzaSyCclQAD744AzjpunjVVc8YYR32UFNkdwgs"
 GOOGLE_TYPES = ['restaurant','food','bar','cafe']
-ALT = "AIzaSyCclQAD744AzjpunjVVc8YYR32UFNkdwgs"
