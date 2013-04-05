7312e7b521b4405a2717428af15f52dacfbedacc
lgorse <lgorse@stanford.edu>
2013-04-05 00:12:36 -0700

    Changed google API key

--- config/initializers/google_api.rb
+++ config/initializers/google_api.rb
@@ -1,2 +1,3 @@
-GOOGLE_API_KEY = "AIzaSyCclQAD744AzjpunjVVc8YYR32UFNkdwgs"
+GOOGLE_API_KEY = "AIzaSyAu3PESBLfwBXKo8511DT78Wg0-S8aNZUU"
 GOOGLE_TYPES = ['restaurant','food','bar','cafe']
+ALT = "AIzaSyCclQAD744AzjpunjVVc8YYR32UFNkdwgs"
