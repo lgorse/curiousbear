f7a2d86f439fd9ef5950c4780a2f3efd28d5604e
lgorse <lgorse@stanford.edu>
2013-04-07 01:16:34 -0700

    Made clearer which section of user relationships user is at, by dynamically highlighting

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,21 +1,38 @@
 $(document).ready(function(){
 
-if($('#relationship_list').length){
-	$.ajax({
+	if($('#relationship_list').length){
+		$.ajax({
 			type: 'GET',
 			url: '/users/'+$('.user_info').attr("id"),
 			dataType: 'json',
 			success: function(response){
 				$('#relationship_list').pageless({ url: '/reviews'
-								, totalPages: response["count"]/5+1
-								, loaderMsg: "loading"
-								, params: {id: response["id"]}
-								, complete: star_ratings
+					, totalPages: response["count"]/5+1
+					, loaderMsg: "loading"
+					, params: {id: response["id"]}
+					, complete: star_ratings
 				});
 			}
-	});		
+		});		
 
-}
+	}
 
 });
+
+$.fn.button_highlight = function(clicked_id){
+	$(this).children('a').each(function(){
+		if ($(this).attr("id") == clicked_id){
+			$(this).removeClass("not_highlighted").addClass("highlighted").unbind('mouseenter mouseleave');
+		}else{
+			$(this).removeClass("highlighted").addClass("not_highlighted").hover(function(){
+				$(this).removeClass("not_highlighted").addClass("highlighted");
+			},
+			function(){
+				$(this).removeClass("highlighted").addClass("not_highlighted");
+			});
+		}
+
+	});
+}
+
 
