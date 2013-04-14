21ecd0b2ef254e1f63f38dec2708eeaa8190040e
lgorse <lgorse@stanford.edu>
2013-04-13 18:04:49 -0700

    added loader image for friends

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,23 @@
 load_listener();
+$(function(){
+$("#add_friends_button").click(function(){
+	big_load_listener();
+});
+});
 
 function load_listener(){
-$(function(){
-	$('.search_form').submit(function(){
-		$('#loading').show().html($("<img>").attr("src", "assets/load.gif"));
-		$('#restaurant_list').hide();
-	});
-		
+	$(function(){
+		$('.search_form').submit(function(){
+			$('#loading').show().html($("<img>").attr("src", "assets/load.gif"));
+			$('#restaurant_list').hide();
+		});
+	});	
+}
 
-});	
+function big_load_listener(){
+	//$("#app_inner").empty().prepend('<img src = "/assets/load.gif"></img>');	
+	$("#app_inner").empty();
+	$("#big_loader").show();
+
 }
 
