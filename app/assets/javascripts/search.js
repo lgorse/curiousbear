d52c6d43a5cc835380c4d9bfd565c5bad398499c
lgorse <lgorse@stanford.edu>
2013-04-15 15:00:22 -0700

    Implemented basic login screen

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,7 +4,7 @@ load_listener();
 function load_listener(){
 	$(function(){
 		$('.search_form').submit(function(){
-			$('#loading').show().html($("<img>").attr("src", "assets/load.gif"));
+			$('#loading').show().html($("<img>").attr("src", "/assets/load.gif"));
 			$('#restaurant_list').hide();
 		});
 	});	
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,9 @@
 class SessionController < ApplicationController
 	include UsersHelper, ReviewsHelper
 	
-	rescue_from NoMethodError, :with => :redirect_to_signin
+	
 	rescue_from Koala::Facebook::AuthenticationError, :with => :logout
-	before_filter :authenticate, :only=>[:home, :info]
+	before_filter :authenticate, :only=>[:home]
 	after_filter :reset_user_session, :only=>[:signin]
 	
 	def signin
@@ -32,7 +32,15 @@ class SessionController < ApplicationController
 	end
 
 	def info
-		@five_friends = @graph.get_connections("me", "friends", :fields => "picture", :limit => 5)
+		if session["user_id"]
+		authenticate 
+		fb_friends = @graph.get_connections("me", "friends", :fields => "picture", :limit => 5)
+		@five_friends = fb_friends.map{|fb_friend| fb_friend['picture']['data']['url']}
+	else
+		@current_user = User.find_by_name("Laurent Gorse")
+		trusted_friends = @current_user.following.limit(5)
+		@five_friends = trusted_friends.map{|trusted_friend| trusted_friend.fb_pic}
+	end
 		respond_to do |format|
 			format.html
 			format.js
